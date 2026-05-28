9ac7ac309d04c82905b581ed31fbdc2c5be8fbaa
Efrik26 <efremov.w26@gmail.com>
2026-05-28 15:34:21 +0300

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -3,6 +3,28 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit('Неверный метод запроса.');
 }
 
+// Защита от спам-ботов: проверяем, что запрос пришёл с вашего сайта
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+$host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
+if (strpos($referer, $host) === false) {
+    exit('Неверный источник запроса.');
+}
+
+// Защита от автоматических отправок: скрытое поле-ловушка
+if (!empty($_POST['honeypot'])) {
+    exit('Спам-запрос.');
+}
+
+// Ограничение длины полей
+$name = isset($_POST['name']) ? mb_substr(htmlspecialchars(trim($_POST['name'])), 0, 100) : '';
+$phone = isset($_POST['phone']) ? mb_substr(htmlspecialchars(trim($_POST['phone'])), 0, 20) : '';
+
+if (empty($name) || empty($phone)) {
+    exit('Заполните обязательные поля.');
+}
+
+// ... дальше ваш текущий код
+
 $name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
 $phone = isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : '';
 $formType = isset($_POST['form_type']) ? $_POST['form_type'] : 'calculator';
